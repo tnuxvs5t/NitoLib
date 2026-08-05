b894421db142ed2e9448dcbeec1ea11d10019624
tnuxvs5t <tnuxvs5t@gmail.com>
2026-08-05 18:00:56 +0900

    examples: probe nfunc on two contest problems

---
+++ examples/cf_1647b_madoka_and_the_elegant_gift.cpp
@@ -0,0 +1,25 @@
+#include "Nitori.h"
+
+int main() {
+    int tests;
+    nin >> tests;
+    while (tests--) {
+        int n, m;
+        nin >> n >> m;
+        nvector<string> grid(n);
+        nrep(row, n)
+            nin >> grid[row];
+
+        auto squares = nfunc(
+            nproduct(nrange(n - 1), nrange(m - 1)),
+            [&](auto cell) {
+                auto [row, column] = cell;
+                return grid[row][column] + grid[row + 1][column] +
+                           grid[row][column + 1] + grid[row + 1][column + 1] -
+                       4 * '0';
+            });
+
+        bool elegant = nnone_of(squares, [](int black) { return black == 3; });
+        nprintln(elegant ? "YES" : "NO");
+    }
+}
